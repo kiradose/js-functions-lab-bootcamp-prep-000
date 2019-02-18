d7a27ddf7e8d9715fd18c3de1ec669ebf1d47548
Claista Guibert <gclaista@gmail.com>
2019-02-18 06:43:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function happyHolidayTo(holiday , name) {
 happyHolidayTo("Independance Day" , "Claista")
 
 function holidayCountdown(holiday , days) {
-  return (`It'/s ${days} days until ${holiday}!`)
+  return (`It'\s ${days} days until ${holiday}!`)
 }
 holidayCountdown( "Mother's Day" , 20)
